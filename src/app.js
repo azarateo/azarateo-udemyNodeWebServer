d554fec4886efb9bbe2ecbecfd271389afd06fac
azarateo <azarateo@gmail.com>
2020-08-15 16:39:40 -0500

    Section 8 Lesson 54

--- src/app.js
+++ src/app.js
@@ -39,12 +39,33 @@ app.get('/help',(req,res)=>{
 })
 
 app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide a search term'
+        })
+    }
+    console.log(req.query)
     res.send(
         {
-            forecast:'It will rain',
-            location:'Bogota, Colombia'
+            address: req.query.address
         }
     )
+
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'Please provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send(
+        {
+            products: []
+        }
+    )
+
 })
 
 app.get('/help/*',(req,res)=>{
