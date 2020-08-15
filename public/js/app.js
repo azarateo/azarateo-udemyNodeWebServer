7d138e4255b7f3049d8f908f56f356f47729dca6
azarateo <azarateo@gmail.com>
2020-08-15 17:45:45 -0500

    Section 8 Lesson 58

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,21 @@
 console.log('Javascript files loaded.');
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log('Error:'+data.error)
-        }else{
-            console.log(data)
-        }
+
+
+const form = document.getElementById('weatherForm')
+const searchBox = document.getElementById('searchBox')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const requestString = 'http://localhost:3000/weather?address=' + searchBox.value
+    fetch(requestString).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log('Error:' + data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.data)
+                console.log(data.address)
+            }
+        })
     })
 })
