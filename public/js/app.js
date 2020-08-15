92d9e040fd9e160039c88ab1dcb924fcf40111ab
azarateo <azarateo@gmail.com>
2020-08-15 17:23:17 -0500

    Section 8 Lesson 57

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log('Javascript files loaded.');
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log('Error:'+data.error)
+        }else{
+            console.log(data)
+        }
+    })
+})
